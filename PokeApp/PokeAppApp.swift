39a3fb704794a5a0f591349ed259b34002235e57
Carlos Alves <carlosaugusto2005@hotmail.com>
2021-02-07 10:47:49 -0300

    Initial Commit

---
+++ PokeApp/PokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeAppApp.swift
+//  PokeApp
+//
+//  Created by Carlos Alves on 07/02/21.
+//
+
+import SwiftUI
+
+@main
+struct PokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
